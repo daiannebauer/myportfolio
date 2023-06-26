e026ddaa336505b605c637276f3df654a7c4e9bf
Daianne Bauer <131558307+daiannebauer@users.noreply.github.com>
2023-06-26 16:10:40 -0700

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import "../styles/portfolio.css";
+import Projects from "./Projects";
 
-const Portfolio = () => {
-  return (
-    <div>Portfolio</div>
-  )
-}
+const Portfolio = () => (
+    <section className="portfolio section">
+       <h2 className="section__title">My Portfolio</h2>
+
+       <Projects />
+    </section>
+)
 
 export default Portfolio
